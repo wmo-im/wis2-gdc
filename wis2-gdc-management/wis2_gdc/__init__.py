16435569838c7c15aa38b173e76b18ef40c3a457
Tom Kralidis <tomkralidis@gmail.com>
2026-02-10 21:11:06 -0500

    back to dev

--- wis2-gdc-management/wis2_gdc/__init__.py
+++ wis2-gdc-management/wis2_gdc/__init__.py
@@ -25,7 +25,7 @@ from wis2_gdc.registrar import register, setup, teardown, unregister
 from wis2_gdc.archive import archive, restore
 from wis2_gdc.sync import sync
 
-__version__ = '0.7.0'
+__version__ = '0.8.dev0'
 
 
 @click.group()
