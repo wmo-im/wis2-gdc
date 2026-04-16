31f789eb90b2053a1e35b2381ccdc14af8fb7631
Tom Kralidis <tomkralidis@gmail.com>
2026-04-16 10:29:15 -0400

    update release version 0.9.0

--- wis2-gdc-management/wis2_gdc/__init__.py
+++ wis2-gdc-management/wis2_gdc/__init__.py
@@ -26,7 +26,7 @@ from wis2_gdc.metrics import metrics
 from wis2_gdc.archive import archive, restore
 from wis2_gdc.sync import sync
 
-__version__ = '0.9.dev0'
+__version__ = '0.9.0'
 
 
 @click.group()
